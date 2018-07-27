b3e18b3f1734c56c1bef0ce60d4e240ac0a17bda
Francesco Ciocchetti <fciocchetti@mintel.com>
2018-07-27 08:47:51 +0100

    Manage exisiting directory that is not a git repo

--- scripts/manage-repodir.sh
+++ scripts/manage-repodir.sh
@@ -8,7 +8,13 @@ set -o pipefail
 
 if [[ -d "$3" ]]; then
   cd $3
-  git pull 
+  set +e
+  git status 2>/dev/null
+  if [[ $? -eq 0 ]]; then
+    git pull 
+  else
+    git clone --single-branch --branch=$1 $2 $3
+  fi
 else
   git clone --single-branch --branch=$1 $2 $3
 fi
